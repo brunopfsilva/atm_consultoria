675f2e5fb985b143a3294edf73c04ce68e8a2261
bruno pf silva <bruno.pfsilva@hotmail.com>
2019-08-24 02:55:46 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:atm_consultoria/pages/homePage.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home: homePage(),
+  debugShowCheckedModeBanner: false,
+));
+
---
+++ lib/pages/homePage.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+
+class homePage extends StatefulWidget {
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text("ATM Consultoria"),
+          backgroundColor: Colors.green,
+        ),
+
+      ),
+    );
+  }
+}
